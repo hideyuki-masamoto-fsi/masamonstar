0ffccbdc80b39caa3bfd28d72c4f50661159dbac
hideyuki-masamoto-fsi <105411774+hideyuki-masamoto-fsi@users.noreply.github.com>
2024-02-16 14:03:54 +0900

    Create test.py

---
+++ src/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import os,sys,json,re,shutil
+
+def main(compo,testtype):
+    print('compo & testtype
+', compo, testtype)
+
+if __name__ == '__main__':
+
+    #メイン処理
+    main(sys.argv[1],sys.argv[2])
